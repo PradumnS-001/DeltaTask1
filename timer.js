c9f44fad177c1584cf5c22a8feda323b2864bb45
Pradumn Singh <pradumnsingh2200@gmail.com>
2025-05-12 22:12:16 +0530

    Finished DeltaTask1

--- timer.js
+++ timer.js
@@ -1,87 +1,37 @@
 let isGamePaused = true;
 let isGameOver = false;
 let isPlacementPhase = true;
-let tempVar, n = null;
-etime = 0;
+let tempVar = null;
+let n = null;
+let etime = 0;
 let move = false;
 const image = document.getElementById("gameMap");
 let sizingRatio = 844 / image.clientHeight;
 let ulkdnodes = [1,2,3,4,5,6];
 let inner = [18,17,16,15,14,13];
 let nodes = [
-    [1,[2,6,7],[2,3,1]],
-    [2,[1,3],[2,1]],
-    [3,[2,4,9],[1,2,1]],
-    [4,[3,5],[2,1]],
-    [5,[4,6,11],[1,1,1]],
-    [6,[1,5],[3,1]],
-    [7,[1,8,12],[1,5,4]],
-    [8,[7,9,17],[5,6,1]],
-    [9,[3,8,10],[1,6,4]],
-    [10,[9,11,15],[4,5,1]],
-    [11,[5,10,12],[1,5,6]],
-    [12,[7,11,13],[4,6,1]],
-    [13,[12,14,18],[1,8,9]],
-    [14,[13,18],[8,8]],
-    [15,[10,14,16],[1,8,9]],
-    [16,[15,17],[9,8]],
-    [17,[8,16,18],[1,8,8]],
-    [18,[13,17],[9,8]]
+    [1,[2,6,7], [34, 424], [2,3,1]],
+    [2,[1,3], [229,88], [2,1]],
+    [3,[2,4,9], [616,88], [1,2,1]],
+    [4,[3,5], [809,424], [2,1]],
+    [5,[4,6,11], [616,758], [1,1,1]],
+    [6,[1,5], [229,758], [3,1]],
+    [7,[1,8,12], [164,424], [1,5,4]],
+    [8,[7,9,17], [293,200], [5,6,1]],
+    [9,[3,8,10], [551,200], [1,6,4]],
+    [10,[9,11,15], [680,424], [4,5,1]],
+    [11,[5,10,12], [551,646], [1,5,6]],
+    [12,[7,11,13], [293,646], [4,6,1]],
+    [13,[12,14,18], [357,535], [1,8,9]],
+    [14,[13,18], [488,535], [8,8]],
+    [15,[10,14,16], [551,424], [1,8,9]],
+    [16,[15,17], [488,311], [9,8]],
+    [17,[8,16,18], [357,311], [1,8,8]],
+    [18,[13,17], [293,424], [9,8]]
 ];
-let x,y = 0;
+let x = 0;
+let y = 0;
 const rect = image.getBoundingClientRect();
-
-image.addEventListener("click", (event)=>{        
-        x = event.pageX - rect.left;
-        y = event.pageY - rect.top;
-        if(!isGamePaused && !isGameOver){
-            n = whichNode();
-
-            if(red.tloc.length === 4 && blue.tloc.length === 4 && isPlacementPhase){
-                isPlacementPhase = false;
-            }
-            if(isPlacementPhase && isnValid(n) == 1){
-                if(blue.isTurn){
-                    blue.tloc.push(n);
-                    allTitansInGame();
-                    score();
-                    placeTitan();
-                    swapTurn();
-                }
-                else if(red.isTurn){
-                    red.tloc.push(n);
-                    allTitansInGame();
-                    score();
-                    placeTitan();
-                    swapTurn();
-                }
-            }
-            else if(isnValid(n) == 2 && !move){
-                tempVar = n;
-                move = true;
-            }  
-            else if(isnValid(n)==1 && move && nodes[tempVar-1][1].includes(n) && blue.isTurn){
-                blue.tloc[blue.tloc.indexOf(tempVar)] = n;
-                move = false;
-                allTitansInGame();
-                score();
-                placeTitan();
-                swapTurn();
-            }
-            else if(isnValid(n)==1 && move && nodes[tempVar-1][1].includes(n) && red.isTurn){
-                red.tloc[red.tloc.indexOf(tempVar)] = n;
-                move = false;
-                allTitansInGame();
-                score();
-                placeTitan();
-                swapTurn();
-            }
-
-            if(isInnerCirc())
-                isGameOver = true;
-            if(isGameOver){EndGame();}
-        }
-    });
 
 class Player{
     constructor(isTurn){
@@ -94,6 +44,63 @@ class Player{
 
 let blue = new Player(true);
 let red = new Player(false);
+
+image.addEventListener("click", (event)=>{        
+        x = event.pageX - rect.left;
+        y = event.pageY - rect.top;
+        if(!isGamePaused && !isGameOver){
+            n = whichNode();
+            extendMap();
+
+            if(red.tloc.length == 4 && blue.tloc.length == 4){
+                isPlacementPhase = false;
+            }
+            let isValidn = isnValid(n);
+            if(isValidn == 0){move = false;}
+            if(isValidn == 1 && !move){
+                if(blue.isTurn && blue.tloc.length != 4){
+                    blue.tloc.push(n);
+                    allTitansInGame();
+                    score();
+                    placeTitan();
+                    swapTurn();
+                }
+                else if(red.isTurn && red.tloc.length != 4){
+                    red.tloc.push(n);
+                    allTitansInGame();
+                    score();
+                    placeTitan();
+                    swapTurn();
+                }
+            }
+            else if(isValidn == 2 && !move){
+                tempVar = n;
+                move = true;
+            }  
+            else if(isValidn==1 && move && nodes[tempVar-1][1].includes(n) && blue.isTurn){
+                blue.tloc[blue.tloc.indexOf(tempVar)] = n;
+                move = false;
+                allTitansInGame();
+                score();
+                placeTitan();
+                swapTurn();
+                tempVar = null;
+            }
+            else if(isValidn==1 && move && nodes[tempVar-1][1].includes(n) && red.isTurn){
+                red.tloc[red.tloc.indexOf(tempVar)] = n;
+                move = false;
+                allTitansInGame();
+                score();
+                placeTitan();
+                swapTurn();
+                tempVar = null;
+            }
+
+            if(isInnerCirc())
+                isGameOver = true;
+        }
+        if(isGameOver){EndGame();}
+    });
 
 function toggle(){
     if(!isGamePaused && !isGameOver){
@@ -112,7 +119,7 @@ function toggle(){
             etime++;
         else if(red.time == 0 || blue.time == 0)
             isGameOver = true;
-        else if(isGamePaused || isGameOver){
+        if(isGamePaused || isGameOver){
             clearInterval(t);
             t = null;
         }
@@ -126,14 +133,32 @@ function reset(){
     isPlacementPhase = true;
     isGameOver = false;
     isGamePaused = true;
+    document.getElementById("rc").style.display = "none";
+    document.getElementById("bc").style.display = "none";
     document.getElementById("redTimer").innerHTML = "5:00";
     document.getElementById("blueTimer").innerHTML = "5:00";
+    document.getElementById("rs").innerHTML = "+0";
+    document.getElementById("bs").innerHTML = "+0";
     document.getElementById("blueTimer").style.backgroundColor = "#bfff0f";
     document.getElementById("blue").style.border = "1px solid #bfff0f";
     document.getElementById("blueInfo").style.backgroundColor = "#bfff0f";
     document.getElementById("redTimer").style.backgroundColor = "whitesmoke";
     document.getElementById("red").style.border = "1px solid rgba(237, 51, 38, 0.8)";
     document.getElementById("redInfo").style.backgroundColor = "whitesmoke";
+    tempVar, n = null;
+    etime = 0;
+    move = false;
+    ulkdnodes = [1,2,3,4,5,6];
+    document.getElementById("reset").style.display = "none";
+
+    document.getElementById("blueTitan1").style.display = "none";
+    document.getElementById("blueTitan2").style.display = "none";
+    document.getElementById("blueTitan3").style.display = "none";
+    document.getElementById("blueTitan4").style.display = "none";
+    document.getElementById("redTitan1").style.display = "none";
+    document.getElementById("redTitan2").style.display = "none";
+    document.getElementById("redTitan3").style.display = "none";
+    document.getElementById("redTitan4").style.display = "none";
 }
 
 function swapTurn(){
@@ -165,18 +190,19 @@ function updateTime(){
     etime = 0;
     const mins = Math.floor(blue.time/60);
     const sec = blue.time - mins * 60;
-    document.getElementById("blueTimer").innerHTML = `${mins}:${sec}`;
+    document.getElementById("blueTimer").innerHTML = mins+":"+sec;
     }
     if(red.isTurn && !isGameOver){
     red.time -= etime;        etime = 0;
     const mins = Math.floor(red.time/60);
     const sec = red.time - mins * 60;
-    document.getElementById("redTimer").innerHTML = `${mins}:${sec}`;
+    document.getElementById("redTimer").innerHTML = mins+":"+sec;
     }
 }
 
 function whichNode(){
-    let ax,ay = 0; 
+    let ax = 0; 
+    let ay = 0;
     ax = x*sizingRatio;
     ay = y*sizingRatio;
 
@@ -199,64 +225,64 @@ function placeTitan(){
     const redTitan3 = document.getElementById("redTitan3");
     const redTitan4 = document.getElementById("redTitan4");
 
-    if(blue.tloc[0] == 0){blueTitan1.style.display = "None";}
+    if(blue.tloc[0] == 0){blueTitan1.style.display = "none";}
     else{
-        blueTitan1.style.display = "Block";
-        blueTitan1.left = nodes[blue.tloc[0]-1][2][0] / sizingRatio;
-        blueTitan1.top = nodes[blue.tloc[0]-1][2][1] / sizingRatio;
+        blueTitan1.style.display = "block";
+        blueTitan1.style.left = ((nodes[blue.tloc[0]-1][2][0]-22) / sizingRatio).toString()+"px";
+        blueTitan1.style.top = ((nodes[blue.tloc[0]-1][2][1]-22) / sizingRatio).toString()+"px";
     }
     if(blue.tloc.length > 1){
-        if(blue.tloc[1] == 0){blueTitan2.style.display = "None";}
+        if(blue.tloc[1] == 0){blueTitan2.style.display = "none";}
         else{
-            blueTitan2.style.display = "Block";
-            blueTitan2.left = nodes[blue.tloc[1]-1][2][0] / sizingRatio;
-            blueTitan2.top = nodes[blue.tloc[1]-1][2][1] / sizingRatio;
+            blueTitan2.style.display = "block";
+            blueTitan2.style.left = ((nodes[blue.tloc[1]-1][2][0]-22) / sizingRatio).toString()+"px";
+            blueTitan2.style.top = ((nodes[blue.tloc[1]-1][2][1]-22) / sizingRatio).toString()+"px";
         }
         if(blue.tloc.length > 2){
-            if(blue.tloc[2] == 0){blueTitan3.style.display = "None";}
+            if(blue.tloc[2] == 0){blueTitan3.style.display = "none";}
             else{
-                blueTitan3.style.display = "Block";
-                blueTitan3.left = nodes[blue.tloc[2]-1][2][0] / sizingRatio;
-                blueTitan3.top = nodes[blue.tloc[2]-1][2][1] / sizingRatio;
+                blueTitan3.style.display = "block";
+                blueTitan3.style.left = ((nodes[blue.tloc[2]-1][2][0]-22) / sizingRatio).toString()+"px";
+                blueTitan3.style.top = ((nodes[blue.tloc[2]-1][2][1]-22) / sizingRatio).toString()+"px";
             }
             if(blue.tloc.length > 3){
-                if(blue.tloc[3] == 0){blueTitan4.style.display = "None";}
+                if(blue.tloc[3] == 0){blueTitan4.style.display = "none";}
                 else{
-                    blueTitan4.style.display = "Block";
-                    blueTitan4.left = nodes[blue.tloc[3]-1][2][0] / sizingRatio;
-                    blueTitan4.top = nodes[blue.tloc[3]-1][2][1] / sizingRatio;
+                    blueTitan4.style.display = "block";
+                    blueTitan4.style.left = ((nodes[blue.tloc[3]-1][2][0]-22) / sizingRatio).toString()+"px";
+                    blueTitan4.style.top = ((nodes[blue.tloc[3]-1][2][1]-22) / sizingRatio).toString()+"px";
                 }
             }
         }
     }
     if(red.tloc.length > 0){
-        if(red.tloc[0] == 0){redTitan1.style.display = "None";}
+        if(red.tloc[0] == 0){redTitan1.style.display = "none";}
         else{
-            redTitan1.style.display = "Block";
-            redTitan1.left = nodes[red.tloc[0]-1][2][0] / sizingRatio;
-            redTitan1.top = nodes[red.tloc[0]-1][2][1] / sizingRatio;
+            redTitan1.style.display = "block";
+            redTitan1.style.left = ((nodes[red.tloc[0]-1][2][0]-22) / sizingRatio).toString()+"px";
+            redTitan1.style.top = ((nodes[red.tloc[0]-1][2][1]-22) / sizingRatio).toString()+"px";
         }
     }
     if(red.tloc.length > 1){
-        if(red.tloc[1] == 0){redTitan2.style.display = "None";}
+        if(red.tloc[1] == 0){redTitan2.style.display = "none";}
         else{
-            redTitan2.style.display = "Block";
-            redTitan2.left = nodes[red.tloc[1]-1][2][0] / sizingRatio;
-            redTitan2.top = nodes[red.tloc[1]-1][2][1] / sizingRatio;
+            redTitan2.style.display = "block";
+            redTitan2.style.left = ((nodes[red.tloc[1]-1][2][0]-22) / sizingRatio).toString()+"px";
+            redTitan2.style.top = ((nodes[red.tloc[1]-1][2][1]-22) / sizingRatio).toString()+"px";
         }
         if(red.tloc.length > 2){
-            if(red.tloc[2] == 0){redTitan3.style.display = "None";}
+            if(red.tloc[2] == 0){redTitan3.style.display = "none";}
             else{
-                redTitan3.style.display = "Block";
-                redTitan3.left = nodes[red.tloc[2]-1][2][0] / sizingRatio;
-                redTitan3.top = nodes[red.tloc[2]-1][2][1] / sizingRatio;
+                redTitan3.style.display = "block";
+                redTitan3.style.left = ((nodes[red.tloc[2]-1][2][0]-22) / sizingRatio).toString()+"px";
+                redTitan3.style.top = ((nodes[red.tloc[2]-1][2][1]-22) / sizingRatio).toString()+"px";
             }
             if(red.tloc.length > 3){
-                if(red.tloc[3] == 0){redTitan4.style.display = "None";}
+                if(red.tloc[3] == 0){redTitan4.style.display = "none";}
                 else{
-                    redTitan4.style.display = "Block";
-                    redTitan4.left = nodes[red.tloc[3]-1][2][0] / sizingRatio;
-                    redTitan4.top = nodes[red.tloc[3]-1][2][1] / sizingRatio;
+                    redTitan4.style.display = "block";
+                    redTitan4.style.left = ((nodes[red.tloc[3]-1][2][0]-22) / sizingRatio).toString()+"px";
+                    redTitan4.style.top = ((nodes[red.tloc[3]-1][2][1]-22) / sizingRatio).toString()+"px";
                 }
             }
         }
@@ -265,6 +291,7 @@ function placeTitan(){
 
 function isnValid(n){
     if(n == 0){return 0;}
+    if(!ulkdnodes.includes(n)){return 0;}
     if(blue.isTurn){
         for(let i of blue.tloc){
             if(i == n)
@@ -294,9 +321,11 @@ function isnValid(n){
 function allTitansInGame(){
     let surrounded = true;
     for(let i of blue.tloc){
-        for(let j of nodes[i-1][1]){
-            if(!red.tloc.includes(j))
+        if(i !=0){
+            for(let j of nodes[i-1][1]){
+            if(!red.tloc.includes(j) && j!=0)
                 surrounded = false;
+        }
         }
         if(surrounded){
             blue.tloc[blue.tloc.indexOf(i)] = 0;
@@ -305,9 +334,11 @@ function allTitansInGame(){
     }
     surrounded = true;
     for(let i of red.tloc){
-        for(let j of nodes[i-1][1]){
-            if(!blue.tloc.includes(j))
+        if(i!=0){
+            for(let j of nodes[i-1][1]){
+            if(!blue.tloc.includes(j)&&j!=0)
                 surrounded = false;
+        }
         }
         if(surrounded){
             red.tloc[red.tloc.indexOf(i)] = 0;
@@ -317,16 +348,19 @@ function allTitansInGame(){
 }
 
 function score(){
-    let redr, blur = [];
+    let redr = [];
+    let blur = [];
+    blue.score = 0;
+    red.score = 0;
 
     for(let i of blue.tloc){
         if(i != 0){blur.push(i);}
     }
-    if(blur.length < 2){isGameOver = true;blue.score = 0;}
+    if(blur.length < 2 && !isPlacementPhase){isGameOver = true;}
     else{
-        for(i of blur){
-            for(j of blur){
-                if(nodes[i-1][1].contains(j)){
+        for(let i of blur){
+            for(let j of blur){
+                if(nodes[i-1][1].includes(j)){
                     blue.score += (nodes[i-1][3][nodes[i-1][1].indexOf(j)]);
                 }
             }
@@ -337,11 +371,11 @@ function score(){
     for(let i of red.tloc){
         if(i != 0){redr.push(i);}
     }
-    if(redr.length < 2){isGameOver = true;red.score = 0;}
+    if(redr.length < 2 && !isPlacementPhase){isGameOver = true;}
     else{
-        for(i of redr){
-            for(j of redr){
-                if(nodes[i-1][1].contains(j)){
+        for(let i of redr){
+            for(let j of redr){
+                if(nodes[i-1][1].includes(j)){
                     red.score += (nodes[i-1][3][nodes[i-1][1].indexOf(j)]);
                 }
             }
@@ -349,25 +383,63 @@ function score(){
         if(red.score != 0){red.score /= 2;}
     }
 
-    document.getElementById("rs").innerHTML = `+${red.score}`;
-    document.getElementById("bs").innerHTML = `+${blue.score}`;
+    document.getElementById("rs").innerHTML = "+" + red.score;
+    document.getElementById("bs").innerHTML = "+" + blue.score;
 }
 
 function isInnerCirc(){
     let loc = (blue.tloc).concat(red.tloc);
     let flag = true;
     for(let i of inner){
-        if(!loc.contains(i))
+        if(!loc.includes(i))
             flag = false;
     }
     return flag;
 }
 
 function EndGame(){
+    if(blue.time == 0|| blue.score <= red.score)
+        redWon();
+    if(red.time == 0|| red.score < blue.score)
+        blueWon();
+    document.getElementById("reset").style.display = "block";
 }
 
 function blueWon(){
+    document.getElementById("bc").style.display = "block";
+    document.getElementById("blueTimer").style.backgroundColor = "#fcef38";
+    document.getElementById("blueInfo").style.backgroundColor = "#fcef38";
+    document.getElementById("redTimer").style.backgroundColor = "whitesmoke";
+    document.getElementById("redInfo").style.backgroundColor = "whitesmoke";   
+    document.getElementById("red").style.border = "1px solid rgba(237, 51, 38, 0.8)";
+    document.getElementById("blue").style.border = "1px solid #fcef38"; 
 }
 
 function redWon(){
+    document.getElementById("rc").style.display = "block";
+    document.getElementById("blueTimer").style.backgroundColor = "whitesmoke";
+    document.getElementById("blueInfo").style.backgroundColor = "whitesmoke";
+    document.getElementById("redTimer").style.backgroundColor = "#fcef38";
+    document.getElementById("redInfo").style.backgroundColor = "#fcef38";
+    document.getElementById("blue").style.border = "1px solid rgba(7, 27, 87, 0.914)";
+    document.getElementById("red").style.border = "1px solid #fcef38";
+}
+
+function extendMap(){
+    let ol = [1,2,3,4,5,6];
+    let ml = [7,8,9,10,11,12];
+    let il = [13,14,15,16,17,18];
+    let lf = true;
+
+    let local = blue.tloc.concat(red.tloc);
+    for(let i of ol){
+        if(!local.includes(i)){lf = false;}
+    }
+    if(lf){ulkdnodes = ol.concat(ml);}
+    lf = true;
+    for(let i of ml){
+        if(!local.includes(i))
+            lf = false;
+    }
+    if(lf){ulkdnodes = (ol.concat(ml)).concat(il);}
 }
